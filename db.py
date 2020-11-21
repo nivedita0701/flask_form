016a9f6ee07662de490b1e3e6196d81d7cb9ca34
u_can_git_it <43105742+nivedita0701@users.noreply.github.com>
2020-11-21 19:04:30 +0530

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_pymongo import pymongo
-from app import app
-
-CONNECTION_STRING = 'mongodb+srv://nivedita:Hello123!@cluster0.mjent.mongodb.net/mydatabase?retryWrites=true&w=majority'
-
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('mydatabase')
-user_collection = pymongo.collection.Collection(db, 'mycollection')
