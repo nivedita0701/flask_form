ee09fe715878095085136cc45a2209550e72888f
u_can_git_it <43105742+nivedita0701@users.noreply.github.com>
2020-11-21 21:15:38 +0530

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
-from flask_wtf import FlaskForm
+from wtforms import Form
 from wtforms import StringField, validators
 from wtforms.validators import DataRequired, Regexp
 
-class myForm(FlaskForm):
+class myForm(Form):
     message = StringField('write a message')
