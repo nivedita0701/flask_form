a8bacc1fc416aec13a1d130d6a43f61b20786864
u_can_git_it <43105742+nivedita0701@users.noreply.github.com>
2020-11-21 19:43:34 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_ngrok import run_with_ngrok
+
 app = Flask(__name__)
+run_with_ngrok(app)
 
 @app.route('/')
 def flask_mongodb_atlas():
     return "flask mongodb atlas!"
+
 
 if __name__ == '__main__':
     app.run()
